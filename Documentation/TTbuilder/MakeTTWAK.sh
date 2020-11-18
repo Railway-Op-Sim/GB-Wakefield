18ebbe1fb58608fceb58d027a31f4a0e2ab913bd
Daniel-Gill <dan23452767645@gmail.com>
2020-11-18 21:52:46 +0000

    Added TTbuilder files

---
+++ Documentation/TTbuilder/MakeTTWAK.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+java -jar "C:\Users\dan23\Desktop\TTbuilder-master\TTbuilder-master\dist\TTBuilder.jar" -o=Wakefield0700.ttb -i=WAK_Head.xml -i=WAK_Westgate.xml
+
+
+read -p "Pause: " name
